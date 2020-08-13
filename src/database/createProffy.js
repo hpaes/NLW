24eb8ebb74871f21821ed6873f6285a8849979a9
Herbert Paes <8264908+hpaes@users.noreply.github.com>
2020-08-13 16:57:56 -0300

    creating database
    creating tables for backend database

---
+++ src/database/createProffy.js
@@ -0,0 +1,57 @@
+module.exports = async function(db, {
+    proffyValue,
+    classValue,
+    classScheduleValues
+}) {
+    // Insert data in proffys table
+    const insertedProffy = await db.run(`
+        INSERT INTO proffys (
+            name,
+            avatar,
+            whatsapp,
+            bio
+        ) VALUES (
+            "${proffyValue.name}",
+            "${proffyValue.avatar}",
+            "${proffyValue.whatsapp}",
+            "${proffyValue.bio}"
+        );
+    `)
+
+    const proffy_id = insertedProffy.lastID
+
+    // Insert data in classes table
+    const insertedClass = await db.run(`
+        INSERT INTO classes (
+            subject,
+            cost,
+            proffy_id
+        ) VALUES (
+            "${classValue.subject}",
+            "${classValue.cost}",
+            "${proffy_id}"
+        );
+    `)
+    
+    const class_id  = insertedClass.lastID
+
+    // Insert data in classSchedule table
+    const insertedAllClassScheduleValues = classScheduleValues.map((classScheduleValue) => {
+        return db.run(`
+            INSERT INTO class_schedule (
+                class_id,
+                weekday,
+                time_from,
+                time_to
+            ) VALUES (
+                "${class_id}",
+                "${classScheduleValue.weekday}",
+                "${classScheduleValue.time_from}",
+                "${classScheduleValue.time_to}"
+            );
+        `)
+    })
+
+    // Here i will execute all db.runs() of classSchedules
+    await Promise.all(insertedAllClassScheduleValues)
+}
