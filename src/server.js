2c1d61f0577452156d765924e60aba696fb7513f
Herbert Paes <8264908+hpaes@users.noreply.github.com>
2020-08-14 14:42:54 -0300

    Update server.js
    refactoring server.js

--- src/server.js
+++ src/server.js
@@ -1,104 +1,15 @@
-const proffys = [
-
-    {
-        name: "Diego Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=400&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4",
-        whatsapp: "899232134",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [
-            0
-        ],
-        time_from: [720],
-        time_to: [1220]
-    },
-    {
-        name: "Mayk Brito",
-        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
-        whatsapp: "8994123412",
-        bio: "Instrutor de Educação Física para iniciantes, minha missão de vida é levar saúde e contribuir para o crescimento de quem se interessar.<br><br>Comecei a minha jornada profissional em 2001, quando meu pai me deu dois alteres de 32kg com a seguinte condição: \"Aprenda a fazer dinheiro com isso!\"",
-        subject: "Educação Física",
-        cost: "40",
-        weekday: [
-            0
-        ],
-        time_from: [720],
-        time_to: [1220]
-    }
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-]
-
-// Functionalities
-
-// add subject name to page-study header
-function getSubject(subjectNumber) {
-    const arrayPosition = +subjectNumber - 1;
-    return subjects[arrayPosition];
-}
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    const filters = req.query
-    return res.render("study.html", {
-        proffys,
-        filters,
-        subjects,
-        weekdays
-    })
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-
-    // check if data is not empty
-    const isNotEmpty = Object.keys(data).length > 0;
-
-    if (isNotEmpty) {
-
-        data.subject = getSubject(data.subject);
-        // add data to the proffys list
-        proffys.push(data);
-
-        return res.redirect("/study");
-    }
-
-    // if data is empty, show page
-    return res.render("give-classes.html", {
-        subjects,
-        weekdays
-    })
-}
-
+// Server
 const express = require('express')
 const server = express()
 
-// configuring nunjucks
+const {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages')
+
+// configuring nunjucks (template engines)
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
     express: server,
@@ -106,10 +17,13 @@ nunjucks.configure('src/views', {
 })
 
 server
-    // config static files (css, scripts, images)
-    .use(express.static("public"))
-    // application paths
-    .get("/", pageLanding)
-    .get("/study", pageStudy)
-    .get("/give-classes", pageGiveClasses)
-    .listen(5500)
+// get data from req.body
+.use(express.urlencoded({ extended: true })) // hide data from url page
+// config static files (css, scripts, images)
+.use(express.static("public"))
+// application paths
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
+.listen(5500)
